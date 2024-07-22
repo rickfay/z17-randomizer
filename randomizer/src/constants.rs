e61d88a87a174de67df67f10d9240e4502e642ea
Rich Fay <therichfay@gmail.com>
2024-07-21 23:24:51 -0400

    update beta version

--- randomizer/src/constants.rs
+++ randomizer/src/constants.rs
@@ -1,5 +1,5 @@
 //! Application Constants
 
 /// Build Version
-pub const VERSION: &str = "v0.4.0 - SNAPSHOT";
+pub const VERSION: &str = "v0.4.0 - Beta Build 2024-07-21";
 pub const CONFIG_FILE_NAME: &str = "config.json";
