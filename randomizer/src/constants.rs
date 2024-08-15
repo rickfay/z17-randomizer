52cc21c0053c5fc1700e8e50f43210c3d2ebe8e2
Rich Fay <therichfay@gmail.com>
2024-08-15 16:36:50 -0400

    bump version

--- randomizer/src/constants.rs
+++ randomizer/src/constants.rs
@@ -1,5 +1,5 @@
 //! Application Constants
 
 /// Build Version
-pub const VERSION: &str = "v0.4.0 - Beta Build 2024-08-06";
+pub const VERSION: &str = "v0.4.0 - Beta Build 2024-08-15";
 pub const CONFIG_FILE_NAME: &str = "config.json";
